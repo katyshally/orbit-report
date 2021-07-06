69b12d12d9ba408afcf1abc6e6f2422526bcacaf
Katy Schoenborn <katyhooper1@gmail.com>
2021-07-05 22:39:50 -0500

    Added warning for Space Debris.

--- orbit-report/src/app/orbit-list/orbit-list.component.ts
+++ orbit-report/src/app/orbit-list/orbit-list.component.ts
@@ -13,7 +13,16 @@ export class OrbitListComponent implements OnInit {
 
   ngOnInit() {
   }
-  objectKeys(satellite: object): string[] {
-    return Object.keys(satellite);
-  }
+  
+  sort(column: string): void {
+    // array.sort modifies the array, sorting the items based on the given compare function
+    this.satellites.sort(function(a: Satellite, b: Satellite): number {
+       if(a[column] < b[column]) {
+          return -1;
+       } else if (a[column] > b[column]) {
+          return 1;
+       }
+       return 0;
+    });
+ }
 }
